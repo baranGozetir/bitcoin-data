def122e87f9dcc2f15a48e77f4d95156099d07da
Baran Gozetir <barangozetir@gmail.com>
2022-09-30 00:18:00 +0300

    getBlocks: draft block hash

--- src/getBlocks.ts
+++ src/getBlocks.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
 export const getLastBlockHash = async () => {
-  return await axios.get("https://blockstream.info/testnet/api/blocks/tip/hash").then((response) => response.data);
+  const initialBlockHeight: number = 0;
+  const lastBlockHeight = await axios.get("https://blockstream.info/testnet/api/blocks/tip/height")
+  console.log(lastBlockHeight)
+  
+
+  if(lastBlockHeight.data > initialBlockHeight){
+    for(let i = initialBlockHeight; i<=lastBlockHeight.data; i++ ){
+      const blockHeight = await axios.get(`https://blockstream.info/testnet/api/block-height/${i}`)
+      console.log(blockHeight.data)
+    }
+  }
+  
 };
