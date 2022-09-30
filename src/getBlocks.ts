eb082d6a5545be6572c6af45ff8aababfabdeeed
Baran Gozetir <barangozetir@gmail.com>
2022-09-30 14:16:38 +0300

    getBlock.ts: information about the block added

--- src/getBlocks.ts
+++ src/getBlocks.ts
@@ -3,13 +3,16 @@ import axios from "axios";
 export const getLastBlockHash = async () => {
   const initialBlockHeight: number = 0;
   const lastBlockHeight = await axios.get("https://blockstream.info/testnet/api/blocks/tip/height")
-  console.log(lastBlockHeight)
+  console.log(lastBlockHeight.data)
   
 
   if(lastBlockHeight.data > initialBlockHeight){
     for(let i = initialBlockHeight; i<=lastBlockHeight.data; i++ ){
       const blockHeight = await axios.get(`https://blockstream.info/testnet/api/block-height/${i}`)
-      console.log(blockHeight.data)
+      console.log('blockHash',blockHeight.data)
+      const blockHash = await axios.get(`https://blockstream.info/testnet/api/block/${blockHeight.data}`)
+      console.log('dataInformation', blockHash.data)
+
     }
   }
   
