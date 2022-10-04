c4fd671626484240bf951cb77de452f1e9cf70b3
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-10-04 21:54:34 +0300

    IRepository: comment removing

--- src/Repository/Interface/IRepository.ts
+++ src/Repository/Interface/IRepository.ts
@@ -3,7 +3,7 @@ import RocksDB from "rocksdb";
 export interface IRepository<T> {
   get: (key: string) => Promise<T | undefined>;
   put: (key: string, value: T) => Promise<T>;
-  del: (key: string) => Promise<string>; // rocksdb id tipi ne
+  del: (key: string) => Promise<string>;
   getMany: (options?: RocksDB.IteratorOptions) => Promise<{ key: string; val: T }[]>;
   deleteAll: () => Promise<void>;
 }
