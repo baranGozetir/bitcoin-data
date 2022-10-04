15581d71bb9152f4771ab616f78cf4dd3676e787
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-10-04 21:11:39 +0300

    IRepository creation

---
+++ src/Repository/IRepository.ts
@@ -0,0 +1,9 @@
+import RocksDB from "rocksdb";
+
+export interface IRepository<T> {
+  get: (key: string) => Promise<T | undefined>;
+  put: (key: string, value: T) => Promise<T>;
+  del: (key: string) => Promise<string>; // rocksdb id tipi ne
+  getMany: (options?: RocksDB.IteratorOptions) => Promise<{ key: string; val: T }[]>;
+  deleteAll: () => Promise<void>;
+}
