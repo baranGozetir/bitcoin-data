83188176820281c343a0d7d168a229f6471cd31e
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-10-04 21:59:31 +0300

    refactoring

--- src/Repository/BlockRepository.ts
+++ src/Repository/BlockRepository.ts
@@ -8,27 +8,15 @@ export class BlockRepository implements IRepository<Block> {
   db = rocksdb("src/block");
   result = new Repository<Block>(this.db);
 
-  open = (resolve: () => void, reject: (arg0: Error) => void): Promise<void> => {
-    return this.result.open(resolve, reject);
-  };
+  open = (resolve: () => void, reject: (arg0: Error) => void): Promise<void> => this.result.open(resolve, reject);
 
-  get = (key: string): Promise<Block | undefined> => {
-    return this.result.get(key);
-  };
+  get = (key: string): Promise<Block | undefined> => this.result.get(key);
 
-  put = (key: string, value: Block): Promise<Block> => {
-    return this.result.put(key, value);
-  };
+  put = (key: string, value: Block): Promise<Block> => this.result.put(key, value);
 
-  del = (key: string): Promise<string> => {
-    return this.result.del(key);
-  };
+  del = (key: string): Promise<string> => this.result.del(key);
 
-  getMany = (options?: RocksDB.IteratorOptions): Promise<{ key: string; val: Block }[]> => {
-    return this.result.getMany(options);
-  };
+  getMany = (options?: RocksDB.IteratorOptions): Promise<{ key: string; val: Block }[]> => this.result.getMany(options);
 
-  deleteAll = (): Promise<void> => {
-    return this.result.deleteAll();
-  };
+  deleteAll = (): Promise<void> => this.result.deleteAll();
 }
