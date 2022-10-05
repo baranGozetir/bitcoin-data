99b0d99cb5b97dadf07dadcd8ccfe9dc1a424b7c
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-10-05 17:08:44 +0300

    BlockController creation

---
+++ src/Controller/BlockController.ts
@@ -0,0 +1,12 @@
+import express, { Response, Request } from "express";
+import { BlockBusiness } from "../Business/BlockBusiness";
+
+const app = express();
+const data = new BlockBusiness();
+
+app.get("/getMany", async (req: any, res: any) => {
+  const result = await data.getMany();
+  res.status(200).send(result);
+});
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
