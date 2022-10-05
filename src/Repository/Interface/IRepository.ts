6400ef72efb4dfdf9394adc3a0e83315166ebda9
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-10-05 15:48:13 +0300

    IRepository: rename  and add method

--- src/Repository/Interface/IRepository.ts
+++ src/Repository/Interface/IRepository.ts
@@ -1,9 +1,10 @@
 import RocksDB from "rocksdb";
 
 export interface IRepository<T> {
+  open: () => Promise<void>;
   get: (key: string) => Promise<T | undefined>;
   put: (key: string, value: T) => Promise<T>;
-  del: (key: string) => Promise<string>;
+  delete: (key: string) => Promise<string>;
   getMany: (options?: RocksDB.IteratorOptions) => Promise<{ key: string; val: T }[]>;
   deleteAll: () => Promise<void>;
 }
