8350932cadb7bf7412530a152bddbde865956b9f
Baran Gozetir <barangozetir@gmail.com>
2022-10-06 17:21:29 +0300

    BlockController: edited

--- src/Controller/BlockController.ts
+++ src/Controller/BlockController.ts
@@ -4,30 +4,30 @@ import { BlockBusiness } from "../Business/BlockBusiness";
 const app = express();
 const data = new BlockBusiness();
 
-app.put("/put", async (req: any, res: any) => {
+app.put("/block", async (req: any, res: any) => {
   const result = await data.put();
   res.status(201).send(result);
 });
 
-app.get("/getMany", async (req: any, res: any) => {
+app.get("/block", async (req: any, res: any) => {
   const result = await data.getMany();
   res.status(200).send(result);
 });
 
-app.get('/get/:id', async (req:any, res:any)=>{
+app.get('/block/:id', async (req:any, res:any)=>{
   let id = req.params.id
   const result = await data.get(id)
   res.send(result)
 })
 
-app.delete('/delete/:id', async (req: any, res: any)=>{
+app.delete('/block/:id', async (req: any, res: any)=>{
   console.log(req)
   let id = req.params.id
   const result = await data.delete(id)
   res.send(result)
 })
 
-app.delete('/delete', async (req:any, res:any)=>{
+app.delete('/block', async (req:any, res:any)=>{
   const result = await data.deleteAll()
   res.send(result)
   console.log(result)
