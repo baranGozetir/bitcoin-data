bebeafc177f8355c0423e1ae294cba2a42339e1f
Baran Gozetir <barangozetir@gmail.com>
2022-10-14 12:36:16 +0300

    IBusinnes: put return type changed

--- src/Business/IBusiness.ts
+++ src/Business/IBusiness.ts
@@ -3,8 +3,10 @@ import RocksDB from "rocksdb";
 export interface IBusiness<T> {
   open: () => Promise<void>;
   get: (key: string) => Promise<T | undefined>;
-  put: () => Promise<void>;
+  put: () => Promise<T[]>;
   delete: (key: string) => Promise<string>;
-  getMany: (options?: RocksDB.IteratorOptions) => Promise<{ key: string; val: T }[]>;
+  getMany: (
+    options?: RocksDB.IteratorOptions
+  ) => Promise<{ key: string; val: T }[]>;
   deleteAll: () => Promise<void>;
 }
