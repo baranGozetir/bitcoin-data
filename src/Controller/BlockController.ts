cf365d5aaae70226fed3589ce622ce1015e629e8
Baran Gozetir <barangozetir@gmail.com>
2022-10-17 18:07:47 +0300

    BlockController: redisegned

--- src/Controller/BlockController.ts
+++ src/Controller/BlockController.ts
@@ -1,38 +1,36 @@
 import express, { Response, Request } from "express";
 import { BlockBusiness } from "../Business/BlockBusiness";
 
-const app = express();
+const block = express.Router();
 const data = new BlockBusiness();
 
-app.put("/block", async (req: any, res: any) => {
+block.put("/", async (req: any, res: any) => {
   const result = await data.put();
   res.status(201).send(result);
 });
 
-app.get("/block", async (req: any, res: any) => {
+block.get("/", async (req: any, res: any) => {
   const result = await data.getMany();
   res.status(200).send(result);
 });
 
-app.get('/block/:id', async (req:any, res:any)=>{
-  let id = req.params.id
-  const result = await data.get(id)
-  res.send(result)
-})
+block.get("/:id", async (req: any, res: any) => {
+  let id = req.params.id;
+  const result = await data.get(id);
+  res.send(result);
+});
 
-app.delete('/block/:id', async (req: any, res: any)=>{
-  console.log(req)
-  let id = req.params.id
-  const result = await data.delete(id)
-  res.send(result)
-})
+block.delete("/:id", async (req: any, res: any) => {
+  console.log(req);
+  let id = req.params.id;
+  const result = await data.delete(id);
+  res.send(result);
+});
 
-app.delete('/block', async (req:any, res:any)=>{
-  const result = await data.deleteAll()
-  res.send(result)
-  console.log(result)
-})
+block.delete("/", async (req: any, res: any) => {
+  const result = await data.deleteAll();
+  res.send(result);
+  console.log(result);
+});
 
-
-
-app.listen(3000, () => console.log(`Example app listening on port 3000!`));
+module.exports = block;
