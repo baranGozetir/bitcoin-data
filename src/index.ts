e6a16738ec76692e5e8e7ccebd200ef7eb22bd7e
Baran Gozetir <barangozetir@gmail.com>
2022-10-19 00:22:28 +0300

    index: required returned to import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import BlockContorller from "./Controller/BlockController.js";
+import TxController from "./Controller/TxController.js";
+
 const app = express();
 
 app.get("/", (req: any, res: any) => {
   res.send("hello world");
 });
 
-app.use("/block", require("./Controller/BlockController"));
-app.use("/tx", require("./Controller/TxController"));
+app.use("/block", BlockContorller);
+app.use("/tx", TxController);
 
 app.listen(3000, () => console.log("server running"));
