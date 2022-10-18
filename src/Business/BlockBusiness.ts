7e2c3280f05952cb5ff2614a5560e5badcaa92b4
Baran Gozetir <barangozetir@gmail.com>
2022-10-19 00:24:19 +0300

    BlockBusines: put redesigned, recursive func added

--- src/Business/BlockBusiness.ts
+++ src/Business/BlockBusiness.ts
@@ -6,35 +6,48 @@ import { IBusiness } from "./IBusiness";
 
 export class BlockBusiness implements IBusiness<BlockDto> {
   result = new BlockRepository();
-
   constructor() {
     this.open();
+    this.fetchBlock();
   }
+
+  fetchBlock = async (i = 0): Promise<any> => {
+    if (i > 5) {
+      return console.log("too much data");
+    } else {
+      const blockhash = [];
+      const blockHash = await axios(
+        "https://blockstream.info/testnet/api/block-height/" + i
+      );
+      blockhash.push(await this.result.put(blockHash.data, blockHash.data));
+
+      return this.fetchBlock(i + 1);
+    }
+  };
 
   open = async () => await this.result.open();
 
-  put = async () => {
-    let blokhash = [];
-    let blockData = [];
-    // const lastBlockHeight = await axios(
-    //   "https://blockstream.info/testnet/api/blocks/tip/height"
-    // );
-    for (let i = 0; i <= 5; i++) {
-      //for (let i = initialBlockHeight; i < lastBlockHeight.data.length; i++)
-      const blockHash = await axios.get(
-        "https://blockstream.info/testnet/api/block-height/" + i
-      );
-      blokhash.push(blockHash.data);
-      const blockInfo = await axios.get(
-        "https://blockstream.info/testnet/api/block/" + blokhash[i]
-      );
+  // put = async () => {
+  //   let blokhash = [];
+  //   let blockData = [];
 
-      blockData.push(
-        await this.result.put(blockInfo.data.height, blockInfo.data)
-      );
-    }
-    return blockData;
-  };
+  //   for (let i = 0; i <= 5; i++) {
+  //     const blockHash = await axios.get(
+  //       "https://blockstream.info/testnet/api/block-height/" + i
+  //     );
+  //     blokhash.push(blockHash.data);
+  //     const blockInfo = await axios.get(
+  //       "https://blockstream.info/testnet/api/block/" + blokhash[i]
+  //     );
+
+  //     blockData.push(
+  //       await this.result.put(blockInfo.data.height, blockInfo.data)
+  //     );
+  //   }
+  //   return blockData;
+  // };
+
+  put = async (key: any, value: any) => this.result.put(key, value);
 
   get = async (key: string) => this.result.get(key);
 
