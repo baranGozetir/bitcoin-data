31801d96ba54a5f6e202bcd78cf43eee03266f6d
Baran Gozetir <barangozetir@gmail.com>
2022-10-19 00:25:09 +0300

    IBusines: put redesigned

--- src/Business/IBusiness.ts
+++ src/Business/IBusiness.ts
@@ -3,7 +3,7 @@ import RocksDB from "rocksdb";
 export interface IBusiness<T> {
   open: () => Promise<void>;
   get: (key: string) => Promise<T | undefined>;
-  put: () => Promise<T[]>;
+  put: (key: string, value: T) => Promise<T>;
   delete: (key: string) => Promise<string>;
   getMany: (
     options?: RocksDB.IteratorOptions
