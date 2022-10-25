67566c719f692856f38f046412834c8bb8c7540f
Baran Gozetir <barangozetir@gmail.com>
2022-10-25 14:49:49 +0300

    index: BlockAnTransaction called

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
-import BlockContorller from "./Controller/BlockController.js";
+import BlockController from "./Controller/BlockController.js";
 import TxController from "./Controller/TxController.js";
+import { BlockAndTransaction } from "./Jobs/BlockAndTransaction";
 
 const app = express();
 
 app.get("/", (req: any, res: any) => {
+  const newBlock = new BlockAndTransaction();
   res.send("hello world");
 });
 
-app.use("/block", BlockContorller);
+app.use("/block", BlockController);
 app.use("/tx", TxController);
 
 app.listen(3000, () => console.log("server running"));
