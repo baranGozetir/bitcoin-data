f8c5b2c87865d6db24b11236287fe185a2382ae1
Baran Gozetir <barangozetir@gmail.com>
2022-10-25 14:51:12 +0300

    BlockAndTransaction: created

---
+++ src/Jobs/BlockAndTransaction.ts
@@ -0,0 +1,107 @@
+import { BlockBusiness } from "../Business/BlockBusiness";
+import { TxBusiness } from "../Business/TxBusiness";
+import axios from "axios";
+
+export class BlockAndTransaction {
+  blockInstance: BlockBusiness;
+  txInstance: TxBusiness;
+
+  constructor() {
+    this.blockInstance = new BlockBusiness();
+    this.txInstance = new TxBusiness();
+    this.openData().then(() => {
+      this.blockAndTransactionSave();
+    });
+  }
+  openData = async () => {
+    await this.blockInstance.open();
+    await this.txInstance.open();
+  };
+  blockAndTransactionSave = async () => {
+    const allBlockData = await this.blockInstance.getMany();
+    const allTxData = await this.txInstance.getMany();
+    console.log("blockdata", allBlockData);
+    console.log("txdata", allTxData);
+
+    if (allBlockData.length === 0) {
+      const blockHash = await axios(
+        "https://blockstream.info/testnet/api/block-height/" + 0
+      );
+
+      const blockData = await axios(
+        "https://blockstream.info/testnet/api/block/" + blockHash.data
+      );
+
+      const blocksTxsId = await axios(
+        "https://blockstream.info/testnet/api/block/" +
+          blockHash.data +
+          "/txids"
+      );
+
+      const transactionPromises = blocksTxsId.data.map(
+        async (element: string) => {
+          return axios(" https://blockstream.info/testnet/api/tx/" + element);
+        }
+      );
+      const transactionData = await Promise.all(transactionPromises);
+
+      let blockKey = blockHash.data;
+      let blockValue = blockData.data;
+
+      await this.blockInstance.put(blockKey, blockValue);
+
+      const transactionDataPromises = transactionData.map(
+        async (element: any) => {
+          let txKey = element.data.txid;
+          let txValue = element.data;
+          return this.txInstance.put(txKey, txValue);
+        }
+      );
+      await Promise.all(transactionDataPromises);
+    } else {
+      const lastData = await this.blockInstance.get(
+        allBlockData[allBlockData.length - 1].key
+      );
+
+      if (lastData) {
+        const blockHash = await axios(
+          "https://blockstream.info/testnet/api/block-height/" +
+            (lastData.height + 1)
+        );
+
+        const blockData = await axios(
+          "https://blockstream.info/testnet/api/block/" + blockHash.data
+        );
+
+        const blocksTxsId = await axios(
+          "https://blockstream.info/testnet/api/block/" +
+            blockHash.data +
+            "/txids"
+        );
+        const transactionPromises = blocksTxsId.data.map(
+          async (element: string) => {
+            return axios(" https://blockstream.info/testnet/api/tx/" + element);
+          }
+        );
+        const transactionData = await Promise.all(transactionPromises);
+
+        let blockKey = blockHash.data;
+        let blockValue = blockData.data;
+
+        await this.blockInstance.put(blockKey, blockValue);
+
+        const transactionDataPromises = transactionData.map(
+          async (element: any) => {
+            let txKey = element.data.txid;
+            let txValue = element.data;
+            return this.txInstance.put(txKey, txValue);
+          }
+        );
+        await Promise.all(transactionDataPromises);
+      } else {
+        console.log("there is no data");
+      }
+    }
+    //this.blockAndTransactionSave();
+  };
+}
