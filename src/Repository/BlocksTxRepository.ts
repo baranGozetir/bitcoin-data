a405dd258c5fac846c78dfd909e52c695cd879fe
Baran Gozetir <barangozetir@gmail.com>
2022-10-25 14:51:29 +0300

    BlockTxRepository: was deleted

--- src/Repository/BlocksTxRepository.ts
+++
@@ -1,9 +0,0 @@
-import rocksdb from "rocksdb";
-import { BlocksTx } from "../Entity/BlocksTx";
-import { Repository } from "./Repository";
-
-export class BlocksTxRepository extends Repository<BlocksTx> {
-  constructor() {
-    super(rocksdb("src/data/BlockTx"));
-  }
-}
