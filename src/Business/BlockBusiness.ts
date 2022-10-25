6a4c890ab5bcf6077ee9317f62e838ead95b867c
Baran Gozetir <barangozetir@gmail.com>
2022-10-25 14:50:47 +0300

    BlockBusines, TxBusines: redesigned.

--- src/Business/BlockBusiness.ts
+++ src/Business/BlockBusiness.ts
@@ -8,11 +8,8 @@ import { keyIn } from "readline-sync";
 
 export class BlockBusiness implements IBusiness<BlockDto> {
   result = new BlockRepository();
-  constructor() {
-    this.open().then(() => {
-      this.fetchBlock();
-    });
-  }
+  constructor() {}
+
   // databasela haberlesmemi sagliyo.
   open = async () => this.result.open();
 
@@ -23,50 +20,14 @@ export class BlockBusiness implements IBusiness<BlockDto> {
   get = async (key: string) => this.result.get(key);
 
   // databasedeki butun datalari cekmemi sagliyo
-  getMany = async (options?: RocksDB.IteratorOptions) =>
-    this.result.getMany(options);
+  getMany = async (options?: RocksDB.IteratorOptions) => {
+    const allBlockData = await this.result.getMany(options);
+    return allBlockData.sort((a, b) => a.val.height - b.val.height);
+  };
 
   // databasede belli bir key degerine gore datayi silmemi sagliyo.
   delete = async (key: string) => this.result.delete(key);
 
   // tum tabloyu ucuruyo
   deleteAll = async () => this.result.deleteAll();
-
-  fetchBlock = async () => {
-    const allData = await this.getMany();
-    allData.sort((a, b) => a.val.height - b.val.height);
-
-    if (allData.length === 0) {
-      const blockHash = await axios(
-        "https://blockstream.info/testnet/api/block-height/" + 0
-      );
-      const blockData = await axios(
-        "https://blockstream.info/testnet/api/block/" + blockHash.data
-      );
-      let key = blockHash.data;
-      let data = blockData.data;
-
-      await this.result.put(key, data);
-    } else {
-      const lastData = await this.get(allData[allData.length - 1].key);
-      if (lastData) {
-        const blockHash = await axios(
-          "https://blockstream.info/testnet/api/block-height/" +
-            (lastData.height + 1)
-        );
-
-        const blockData = await axios(
-          "https://blockstream.info/testnet/api/block/" + blockHash.data
-        );
-        let key = blockHash.data;
-        let data = blockData.data;
-        await this.result.put(key, data);
-      } else {
-        console.log("there is no last data information");
-      }
-    }
-    console.log("111", allData);
-
-    //this.fetchBlock();
-  };
 }
--- src/Business/TxBusiness.ts
+++ src/Business/TxBusiness.ts
@@ -7,11 +7,7 @@ import { IBusiness } from "./IBusiness";
 export class TxBusiness implements IBusiness<TxDto> {
   result = new TxRepository();
 
-  constructor() {
-    this.open().then(() => {
-      this.getTx();
-    });
-  }
+  constructor() {}
   open = async () => await this.result.open();
 
   put = async (key: any, value: any) => this.result.put(key, value);
@@ -24,55 +20,4 @@ export class TxBusiness implements IBusiness<TxDto> {
   delete = async (key: string) => this.result.delete(key);
 
   deleteAll = async () => this.result.deleteAll();
-
-  getTx = async () => {
-    const allTxData = await this.getMany();
-    allTxData.sort(
-      (a, b) => a.val.status.block_height - b.val.status.block_height
-    );
-    console.log("txxler", allTxData[allTxData.length - 1]);
-
-    if (allTxData.length === 0) {
-      const blockHash = await axios(
-        "https://blockstream.info/testnet/api/block-height/" + 0
-      );
-      const blocksTxsId = await axios(
-        "https://blockstream.info/testnet/api/block/" +
-          blockHash.data +
-          "/txids"
-      );
-      blocksTxsId.data.forEach(async (element: string) => {
-        const TxInfo = await axios(
-          " https://blockstream.info/testnet/api/tx/" + element
-        );
-        let key = blocksTxsId.data;
-        let data = TxInfo.data;
-        await this.result.put(key, data);
-      });
-    } else {
-      const lastTxData = await this.get(allTxData[allTxData.length - 1].key);
-      if (lastTxData) {
-        const lastBlockHeight = lastTxData.status.block_height;
-        const blockHash = await axios(
-          "https://blockstream.info/testnet/api/block-height/" +
-            (lastBlockHeight + 1)
-        );
-        const blocksTxsId = await axios(
-          "https://blockstream.info/testnet/api/block/" +
-            blockHash.data +
-            "/txids"
-        );
-        blocksTxsId.data.forEach(async (element: string) => {
-          const TxInfo = await axios(
-            " https://blockstream.info/testnet/api/tx/" + element
-          );
-          let key = blocksTxsId.data;
-          let data = TxInfo.data;
-          await this.result.put(key, data);
-        });
-      } else {
-        ("there is no data");
-      }
-    }
-  };
 }
