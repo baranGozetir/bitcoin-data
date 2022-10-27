c23866143038a84275e31cfb0577e7871e013c0c
Baran Gozetir <barangozetir@gmail.com>
2022-10-27 19:37:00 +0300

    BlockController: put fixed. query.params are added

--- src/Controller/BlockController.ts
+++ src/Controller/BlockController.ts
@@ -4,15 +4,27 @@ import { BlockBusiness } from "../Business/BlockBusiness";
 const block = express.Router();
 const data = new BlockBusiness();
 
-// block.put("/", async (req: any, res: any) => {
-//   const result = await data.put();
-//   res.status(201).send(result);
-// });
+block.put("/", async (req: any, res: any) => {
+  const key = req.body.key;
+  const value = req.body.value;
+  const result = await data.put(key, value);
+  res.status(201).send(result);
+});
 
 block.get("/", async (req: any, res: any) => {
   const result = await data.getMany();
-  res.status(200).send(result);
-  console.log(result);
+  res.send(
+    result.find((element) => element.val.height == req.query.height) ||
+      result.find((element) => element.key == req.query.hash) ||
+      result.find((element) => element.val.timestamp == req.query.timestamp) ||
+      result.find((element) => element.val.version == req.query.version) ||
+      result.find((element) => element.val.size == req.query.size) ||
+      result.find((element) => element.val.tx_count == req.query.tx_count) ||
+      result.find(
+        (element) => element.val.difficulty == req.query.difficulty
+      ) ||
+      result.find((element) => element.val.id == req.query.id)
+  );
 });
 
 block.get("/:id", async (req: any, res: any) => {
